c9f4d41c4d5978c7ad5ce34a174a16857cd522b0
ShrastiJain-1305 <shrastijain1305@gmail.com>
2025-11-18 00:24:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var ri = document.querySelector(".ri");
+var submit = document.querySelector("#btn");
+var form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var name = e.target.name.value;
+  var model = e.target.model.value;
+  var url = e.target.url.value;
+  if (name && model && url) {
+    const card = document.createElement("div");
+    card.classList.add("smallbox");
+    card.innerHTML = `
+      <div class="top">
+        <img src="${url}">
+      </div>
+      <div class="bottom">
+        <h1>${name}</h1>
+        <h4>${model}</h4>
+        <button class="remove">Remove</button>
+      </div>
+    `;
+    var btn = card.querySelector(".remove");
+    ri.appendChild(card);
+
+    btn.addEventListener("click", function () {
+      card.remove();
+    });
+  } else {
+    alert("Enter all required details");
+  }
+  form.reset();
+});
